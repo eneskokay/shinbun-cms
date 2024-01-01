7d6c033e17b8dcaa3b1f99730a92ef3bba18e203
enes_kokay <eneskokaytr@gmail.com>
2024-01-01 17:22:16 +0300

    started to auth page

---
+++ src/app/auth-admin/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Login from "@/pageComponents/auth/login/login";
+
+function AuthAdmin() {
+    return (
+        <div>
+            <Login />
+        </div>
+    );
+}
+
+export default AuthAdmin;
---
+++ src/pageComponents/auth/login/login.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Input} from "@mui/base";
+
+function Login() {
+
+    return (
+        <div>
+            <Input defaultValue={} />
+        </div>
+    );
+}
+
+export default Login;
